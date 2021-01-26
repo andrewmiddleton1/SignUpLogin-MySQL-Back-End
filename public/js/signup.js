32beb254923a1421aa83b45b0b099f3c7f21cee5
andrewmiddleton1 <66552488+andrewmiddleton1@users.noreply.github.com>
2021-01-27 07:39:55 +1100

    Final Project Back End with First Name and Last Name not working

---
+++ public/js/signup.js
@@ -0,0 +1,52 @@
+$(document).ready(function () {
+  // Getting references to our form and input
+  var signUpForm = $("form.signup");
+  var emailInput = $("input#email-input");
+  var passwordInput = $("input#password-input");
+  var firstNameInput = $("input#first-name-input");
+  var lastNameInput = $("input#last-name-input");
+
+  // When the signup button is clicked, we validate the email and password are not blank
+  signUpForm.on("submit", function (event) {
+    event.preventDefault();
+    var userData = {
+      email: emailInput.val().trim(),
+      password: passwordInput.val().trim(),
+      first_name: firstNameInput.val().trim(),
+      last_name: lastNameInput.val().trim(),
+    };
+
+    console.log(userData);
+
+    if (!userData.email || !userData.password) {
+      return;
+    }
+    // If we have an email and password, run the signUpUser function
+    signUpUser(userData.email, userData.password, userData.first_name, userData.last_name);
+    emailInput.val("");
+    passwordInput.val("");
+    firstNameInput.val("");
+    lastNameInput.val("");
+  });
+
+  // Does a post to the signup route. If successful, we are redirected to the members page
+  // Otherwise we log any errors
+  async function signUpUser(email, password, first_name, last_name) {
+    var res = await $.post("/api/signup", {
+      email: email,
+      password: password,
+      first_name: first_name,
+      last_name: last_name
+    })
+      .then(function (data) {
+        window.location.replace("/members");
+        // If there's an error, handle it by throwing up a bootstrap alert
+      })
+      .catch(handleLoginErr);
+  }
+
+  function handleLoginErr(err) {
+    $("#alert .msg").text(err.responseJSON);
+    $("#alert").fadeIn(500);
+  }
+});
